6072532343eba936b3509297d3330450d8f3754a
amosplentisoft <amos@plentisoft.com>
2022-02-28 14:43:56 +0800

    add bootstrap dependencies

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,16 @@ import HelloWorld from "./components/HelloWorld.vue";
   <header>
     <div class="wrapper">
       <HelloWorld msg="Hello World!" />
+      <button
+        class="btn btn-primary"
+        data-bs-target="#collapseTarget"
+        data-bs-toggle="collapse"
+      >
+        Bootstrap collapse
+      </button>
+      <div class="collapse py-2" id="collapseTarget">
+        This is the toggle-able content!
+      </div>
     </div>
   </header>
 </template>
