b97f62da1bc4d0ca10ec5e01068b63bae97504a4
amoschoomy <acho0057@student.monash.edu>
2022-02-28 22:57:26 +0800

    added routers

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,37 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import AboutMeVue from "./components/AboutMe.vue";
+import ContactMeVue from "./components/ContactMe.vue";
+import ExpAndEduVue from "./components/ExpAndEdu.vue";
+import HomeVue from "./components/Home.vue";
 const routerHistory = createWebHistory();
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeVue,
+  },
+
+  {
+    path: "/about-me",
+    name: "About Me",
+    component: AboutMeVue,
+  },
+  {
+    path: "/experience-and-education",
+    name: "Experience and Education",
+    component: ExpAndEduVue,
+  },
+  {
+    path: "/contact-me",
+    name: "Contact Me",
+    component: ContactMeVue,
+  },
+];
+
+const router = createRouter({
+  history: routerHistory,
+  routes,
+});
+
+export default router;
