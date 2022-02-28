8d0ae2d4f4e3300ca3ec3016bd3af7d67af6c24f
amosplentisoft <amos@plentisoft.com>
2022-02-28 22:32:50 +0800

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="Hello World!" />
-      <button
-        class="btn btn-primary"
-        data-bs-target="#collapseTarget"
-        data-bs-toggle="collapse"
-      >
-        Bootstrap collapse
-      </button>
-      <div class="collapse py-2" id="collapseTarget">
-        This is the toggle-able content!
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarsExample11"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+      <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+        <div class="navbar-nav">
+          <li class="nav-item active mx-5">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item mx-5">
+            <a class="nav-link" href="#">About Me</a>
+          </li>
+          <li class="nav-item mx-5">
+            <a class="nav-link" href="#">Experience and Education</a>
+          </li>
+          <li class="nav-item mx-5">
+            <a class="nav-link" href="#">Contact Me</a>
+          </li>
+        </div>
       </div>
     </div>
-  </header>
+  </nav>
 </template>
