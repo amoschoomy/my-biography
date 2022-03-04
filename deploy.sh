738a90cf16a52bf517be46a5500fd132054cec9f
amoschoomy <acho0057@student.monash.edu>
2022-03-04 13:31:35 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,17 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git add -A
+git add deploy.sh
+git add dist -f
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/amoschoomy/my-biography.git master:gh-pages
+git subtree push --prefix dist https://github.com/amoschoomy/my-biography.git main:gh-pages
 
-cd -
+cd /
